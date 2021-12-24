bb00b40b7f58fdb3c14f98dc7f634c9008cd979b
Agustin <agustinlodola@gmail.com>
2021-12-24 12:19:26 -0300

    version 2 ts

--- app2.ts
+++ app2.ts
@@ -6,11 +6,19 @@ function getData() {
     //document.body.removeChild(document.getElementById("aviso"));
     if (name == "") {
         document.getElementById("name").focus;
-       
+        let mensaje = document.createElement('h1');
+        mensaje.textContent = "completa el nombre, por favor";
+        mensaje.setAttribute("id", "aviso");
+
+        document.body.appendChild(mensaje);
     }
 
     if (age == "") {
         document.getElementById("age").focus;
+        let mensaje = document.createElement('h1');
+        mensaje.textContent = "completa la edad, por favor";
+        mensaje.setAttribute("id", "aviso");
+        document.body.appendChild(mensaje);
     }
 
     console.log(name + " " + age);
