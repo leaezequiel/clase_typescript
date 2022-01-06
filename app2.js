bd64547ac833480a476d1243aefe4a3b9d28b5b0
Lean <lea.ezequiel@live.com>
2022-01-06 11:05:47 -0300

    ejercicio con resolucion

--- app2.js
+++ app2.js
@@ -1,29 +1,40 @@
 function getData() {
     var name = document.getElementById("nombre").value;
     var age = document.getElementById("edad").value;
+    //CREE UNA VARIABLE BOOLEANA QUE FUNCIONA COMO "SEGURO", SE ACTIVA CUANDO YA HAY UN H1 
+    var puedeEscribir=true;
+    
     try {
         // código...
         if (document.body.children.namedItem("aviso") != null)
             document.body.removeChild(document.getElementById("aviso"));
+            //DESACTIVAMOS EL SEGURO COMO AL PRINCIPIO
+            var puedeEscribir=true;
     }
     catch (err) {
         // manipulación de error
         console.log("no hubo error anteriormente");
     }
+
     //document.body.removeChild(document.getElementById("aviso"));
-    if (name == "") {
+    //LA VARIABLE SEGURO SE ACTIVA 
+    if (name == "" && puedeEscribir == true ) {
         document.getElementById("nombre").focus;
         var mensaje = document.createElement('h1');
         mensaje.textContent = "completa el nombre, por favor";
+        puedeEscribir= false;
         mensaje.setAttribute("id", "aviso");
         document.body.appendChild(mensaje);
     }
-    if (age == "") {
+
+    //LA VARIABLE SEGURO SE ACTIVA 
+    if (age == "" && puedeEscribir == true) {
         document.getElementById("edad").focus;
         var mensaje = document.createElement('h1');
         mensaje.textContent = "completa la edad, por favor";
+        puedeEscribir= false;
         mensaje.setAttribute("id", "aviso");
         document.body.appendChild(mensaje);
     }
-    console.log(name + " " + age);
+    console.log(name + " " + age + " ");
 }
